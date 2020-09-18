23329ab46a15b2550fee6d0830434cd64de6af60
brunosp1024 <bruno@DESKTOP-31K2E3S>
2020-09-18 00:52:40 -0300

    DbContext implementado

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    //Interface base para assinatura de todas as classes repositórios das entidades.
+    public interface IBaseRepositorio<TEntity> : IDisposable where TEntity : class //onde TEntity é uma classe 
+    {
+        void Adicionar(TEntity entity);
+        TEntity ObterPorId(int Id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity entity);
+        void Deletar(TEntity entity);
+    }
+}
--- QuickBuy.Dominio/Entidades/Produto.cs
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Produto : Entidade
+    public class Produto : Entidade //Herda da classe Entidade
     {
 
         public int id { get; set; }
@@ -12,9 +12,9 @@ namespace QuickBuy.Dominio.Entidades
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
 
-        public override void Validate()
+        public override void Validate() //Método de validação
         {
-            if (string.IsNullOrEmpty(Nome))
+            if (string.IsNullOrEmpty(Nome)) //Se o atributo Nome for nulo ou vazio
                 AdicionarMensagem("O Nome do produto não foi informado!");
 
             if (Preco == 0)
